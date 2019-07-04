2ff2db41dbcec6295117fc7521edd4afb6cbed93
Cryptopurr Columbus <cryptopurrcolumbus@gmail.com>
2019-07-04 22:46:55 +0530

    css: basic styling
    Signed-off-by: Cryptopurr Columbus <cryptopurrcolumbus@gmail.com>

--- js/main.js
+++ js/main.js
@@ -13,44 +13,51 @@ function getUserdata(page=PAGE) {
       if (!response.ok) {
         throw Error(response.statusText);
       }
-      let a = nextreq(response);
-      console.log(a);
-      return Promise.all([response.json(), a]);
+      // let _shouldAddNextButton = shouldAddNextButton(response);
+			// return Promise.all([response.json(), _shouldAddNextButton]);
+			return response.json()
     })
-    .then(function([data, a]) {
-      clearContent();
-      console.log(a);
-      appendData(data);
-      moreM(data, a);
+    .then(function(data) {
+			handleSuccessfulGithubResponse(data)
     })
     .catch(function(err) {
-      clearContent();
-      let errorBox = document.querySelector('#errorBox');
-      errorBox.innerHTML = err;
+			handleFailedGithubResponse(err)
     });
 }
 
-function moreM(data, a) {
-  let more = document.querySelector('#moreContent');
-  console.log(data);
+function handleSuccessfulGithubResponse(data) {
+		clearContent();
+		appendData(data);
+		showButtons(data);
+}
+
+function handleFailedGithubResponse(err) {
+		clearContent();
+		let errorBox = document.querySelector('#errorBox');
+		errorBox.innerHTML = err;
+}
+
+function showButtons(data) {
+	let more = document.querySelector('#moreContent');
+	if (PAGE > 1) {
+		// show the previous button
+		// document.getElementById("previous").style.display = 'inline';
+		console.log(document.getElementById("previous"))
+	}
   if (data.length < 30) {
-    console.log('hi');
     more.innerHTML = `<h2>Showing ${
       data.length
     } repositories. No more repositories are available</h2>`;
   } else {
-    console.log('heya');
     more.innerHTML = `<h2>Showing ${
       data.length
-    } repositories.Click next button to load more repositories</h2> <button type="submit" id="prev">Prev</button> <button type="submit" id="next">Next</button>`;
-    request(a);
+		} repositories. Click next button to load more repositories</h2>`;
+		// show the next button
+		console.log(document.getElementById("next"));
+		// document.getElementById("next").style.display = 'inline';
   }
 }
 
-function handleGithubResponse(response) {
-  clearContent();
-  addRepoInfoHTML(response);
-}
 
 function appendData(data) {
   var mainContainer = document.getElementById('output');
@@ -79,31 +86,35 @@ function clearContent() {
   errorBox.innerHTML = '';
 }
 
-function nextreq(response) {
-  console.log(response);
-  let z = response.headers.get('Link');
-  if (typeof z === 'string') {
-    console.log(z);
-    let fs = z.indexOf('<');
-    let fe = z.indexOf('>');
-    let ss = z.lastIndexOf('<');
-    let pe = z.lastIndexOf('>');
-    console.log(fs + 1, fe - 1, ss + 1, pe - 1);
-    let start = z.slice(fs + 1, fe);
-    let end = z.slice(ss + 1, pe);
-    console.log('start:', start);
-    console.log('end:', end);
-    console.log(start === end);
-    return [start === end];
-  }
-}
+// function shouldAddNextButton(response) {
+//   let z = response.headers.get('Link');
+//   if (typeof z === 'string') {
+//     let fs = z.indexOf('<');
+//     let fe = z.indexOf('>');
+//     let ss = z.lastIndexOf('<');
+//     let pe = z.lastIndexOf('>');
+//     let start = z.slice(fs + 1, fe);
+//     let end = z.slice(ss + 1, pe);
+//     return [start === end];
+//   }
+// }
 
-function request() {
-  let nextBtn = document.querySelector('#next');
-  nextBtn.addEventListener('click', clickBtn);
-}
 
-function clickBtn() {
+// Register the event handlers for next and previous button
+let nextBtn = document.querySelector('#next');
+nextBtn.addEventListener('click', increasePageNumber);
+
+let previousButton = document.querySelector('#previous');
+previousButton.addEventListener('click', decreasePageNumber);
+
+
+// Event handlers for next and previous buttons
+function increasePageNumber() {
 	PAGE += 1;
+	getUserdata();
+}
+
+function decreasePageNumber() {
+	PAGE -= 1;
 	getUserdata();
 }
