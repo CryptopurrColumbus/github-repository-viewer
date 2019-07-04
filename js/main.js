e3c213285299694b8851e745f77ce1f951a0a053
Cryptopurr Columbus <cryptopurrcolumbus@gmail.com>
2019-07-04 21:15:57 +0530

    js: better names and decorations
    Signed-off-by: Cryptopurr Columbus <cryptopurrcolumbus@gmail.com>

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@ const getUserName = () => {
 };
 
 function getUserdata() {
-	fetch(`https://api.github.com/users/${getUserName()}/repos`)
+	var username = getUserName();
+	fetch(`https://api.github.com/users/${username}/repos`)
 		.then(function(response) {
       // console.log( response.headers.get('Link'));
       if (!response.ok) {
@@ -26,29 +27,9 @@ function getUserdata() {
 			let errorBox = document.querySelector('#errorBox');
 			errorBox.innerHTML = err;
 		});
-
-	function appendData(data) {
-		var mainContainer = document.getElementById('output');
-		var inputField = document.querySelector('#username');
-		let a = `<h1>${inputField.value}: User Repositories</h1>`;
-		console.log(a);
-
-		for (var i = 0; i < data.length; i++) {
-			let output = `<h1><a href=${data[i].html_url} >${
-				data[i].name
-			}</a></h1> <h2> Description:${data[i].description}</h2><h3> Is a fork : ${
-				data[i].fork
-			} </h3> <h4> Language: ${data[i].language} <p> Watched : ${
-				data[i].watchers
-			}<p> <p> Starred : ${data[i].stargazers_count}<p> <p>Forked : ${
-				data[i].forks
-			}<p>`;
-
-			a += output;
-		}
-		mainContainer.innerHTML = a;
-	}
 }
+
+
 function moreM(data,a) {
 	let more = document.querySelector('#moreContent');
 	console.log(data);
@@ -67,12 +48,33 @@ function moreM(data,a) {
 	}
 }
 
-function clearContent() {
-	var mainContainer = document.getElementById('output');
-	mainContainer.innerHTML = '';
-	let errorBox = document.querySelector('#errorBox');
-	errorBox.innerHTML = '';
+function addRepoInfoHTML(data) {
+  var mainContainer = document.getElementById('output');
+  var inputField = document.querySelector('#username');
+  let a = `<h1>${inputField.value}: User Repositories</h1>`;
+
+  for (var i = 0; i < data.length; i++) {
+    let output = `<h1><a href=${data[i].html_url} >${
+      data[i].name
+    }</a></h1> <h2> Description:${data[i].description}</h2><h3> Is a fork : ${
+      data[i].fork
+    } </h3> <h4> Language: ${data[i].language} <p> Watched : ${
+      data[i].watchers
+    }<p> <p> Starred : ${data[i].stargazers_count}<p> <p>Forked : ${
+      data[i].forks
+    }<p>`;
+    a += output;
+  }
+  mainContainer.innerHTML = a;
 }
+
+function clearContent() {
+  var mainContainer = document.getElementById('output');
+  mainContainer.innerHTML = '';
+  let errorBox = document.querySelector('#errorBox');
+  errorBox.innerHTML = '';
+}
+
 function nextreq(response) {
 	console.log(response);
   let z = response.headers.get('Link');
